4161bcdf2aae0ee3c287e49d1b32858d1ba30b44
Azukee <theorybreakerhacks@gmail.com>
2021-06-20 17:48:59 +0200

    Fix #1

---
+++ Maple Server/Crypto/Wrapper/RSAEncrypt.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <string>
+#include "../base64.h"
+#include "../rsa.h"
+#include "../osrng.h"
+#include "../hex.h"
+#include "../files.h"
+#include "../pssr.h"
+
+using namespace CryptoPP;
+
+class RSAEncrypt
+{
+private:
+	std::string privKey;
+	std::string privKeyDecoded;
+
+	void decodePrivKeyBase64();
+public:
+	RSAEncrypt();
+
+	std::string Encrypt(std::string input);
+};
