b45ca6de60a3a707933e19d0cacdcb1f20534f92
Azukee <theorybreakerhacks@gmail.com>
2021-06-20 22:23:28 +0200

    Fix up some shitty bugged linux code and add _CRT_SECURE_NO_WARNINGS

---
+++ Maple Server/TCP/tcp_server.h
@@ -0,0 +1,47 @@
+#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <thread>
+#include <functional>
+#include <cstring>
+#include <errno.h>
+#include <iostream>
+#include "client.h"
+#include "server_observer.h"
+#include "pipe_ret_t.h"
+#pragma comment(lib, "Ws2_32.lib")
+#include <io.h>
+#include <process.h>
+#include <WS2tcpip.h>
+#include <winsock.h>
+#include <WinSock2.h>
+typedef unsigned int uint;
+
+#define MAX_PACKET_SIZE 4096
+
+class TcpServer
+{
+private:
+    int m_sockfd;
+    struct sockaddr_in m_serverAddress;
+    struct sockaddr_in m_clientAddress;
+    fd_set m_fds;
+    std::vector<Client> m_clients;
+    std::vector<server_observer_t> m_subscibers;
+    std::thread * threadHandle;
+
+    void publishClientMsg(const Client & client, const char * msg, size_t msgSize);
+    void publishClientDisconnected(const Client & client);
+    void receiveTask(/*void * context*/);
+public:
+    pipe_ret_t start(int port);
+    Client acceptClient(uint timeout);
+    bool deleteClient(Client & client);
+    void subscribe(const server_observer_t & observer);
+    void unsubscribeAll();
+    pipe_ret_t sendToAllClients(const char * msg, size_t size);
+    pipe_ret_t sendToClient(const Client & client, const char * msg, size_t size);
+    pipe_ret_t finish();
+    void printClients();
+};
